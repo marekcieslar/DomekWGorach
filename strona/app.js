d871c6b7972cc938cbc89de0990428a1f597b14e
marek <marek.cieslar@gmail.com>
2017-07-09 13:41:49 +0200

    add and remove nav-opened toggle class on smaller or bigger screen

--- strona/app.js
+++ strona/app.js
@@ -6,10 +6,30 @@ function toggleClass(element, clas) {
     element.classList.toggle(clas);
 };
 
-hamburger.addEventListener('click', menuClick);
-for (let i = 0; i< menuItems.length; i++) {
-    menuItems[i].addEventListener('click', menuClick);
+function addMenuClick() {
+    hamburger.addEventListener('click', menuClick);
+    for (let i = 0; i < menuItems.length; i++) {
+        menuItems[i].addEventListener('click', menuClick);
+    }
 }
+
+function removeMenuClick() {
+    hamburger.removeEventListener('click', menuClick);
+    for (let i = 0; i < menuItems.length; i++) {
+        menuItems[i].removeEventListener('click', menuClick);
+    }
+}
+if (document.body.clientWidth <640) {
+    addMenuClick();
+}
+
+window.addEventListener('resize', (e) => {
+    if (e.currentTarget.innerWidth > 639) {
+        removeMenuClick();
+    } else {
+        addMenuClick();
+    }
+});
 
 function changeLogoColor(color) {
     document
@@ -23,8 +43,7 @@ function menuClick() {
     toggleClass(header, 'nav-opened');
     if (header.classList.contains('nav-opened')) {
         changeLogoColor('#000000');
-    }
-    else {
+    } else {
         changeLogoColor('#FFFFFF');
     }
 }
