b222fdcdff43bed20f0519427285a7b2a7c5874e
marek <marek.cieslar@gmail.com>
2017-07-09 09:20:07 +0200

    open and close menu, change logo.svg color from JS

--- strona/app.js
+++ strona/app.js
@@ -1,19 +1,30 @@
 let hamburger = document.getElementById('hamburger');
 let header = document.getElementById('page-header');
-let menuItems = document.getElementsByClassName('menu-link');
+let menuItems = document.getElementsByClassName('menu-item');
 
 function toggleClass(element, clas) {
     element.classList.toggle(clas);
 };
 
-hamburger.addEventListener('click', () => {
-    toggleClass(header, 'open');
-});
+hamburger.addEventListener('click', menuClick);
+for (let i = 0; i< menuItems.length; i++) {
+    menuItems[i].addEventListener('click', menuClick);
+}
 
-// add on click for every link
-// need only if it is SPA
-for (let i = 0; i < menuItems.length; i++) {
-    menuItems[i].addEventListener('click', () => {
-        toggleClass(header, 'open');
-    });
+function changeLogoColor(color) {
+    document
+        .querySelector(".header-logo")
+        .contentDocument
+        .getElementById("logo")
+        .setAttribute('fill', color)
+}
+
+function menuClick() {
+    toggleClass(header, 'nav-opened');
+    if (header.classList.contains('nav-opened')) {
+        changeLogoColor('#000000');
+    }
+    else {
+        changeLogoColor('#FFFFFF');
+    }
 }
